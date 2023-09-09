9f47ad2617cc2dc1864bd50e4f0c13cb6aaae880
JordanMills233 <jordanmills233@gmail.com>
2023-09-09 19:43:56 +0100

    Updated property names

--- src/app/components/EditIcon.tsx/EditIcon.tsx
+++ src/app/components/EditIcon.tsx/EditIcon.tsx
@@ -16,11 +16,11 @@ export const EditIcon = ({ onClick }: editProps) => {
         height="25px"
         width="25px"
       >
-        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
         <g
           id="SVGRepo_tracerCarrier"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         ></g>
         <g id="SVGRepo_iconCarrier">
           {" "}
