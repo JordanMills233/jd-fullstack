ddcf3c9e76bc54bd66584cc413081ed1ea8ca927
JordanMills233 <jordanmills233@gmail.com>
2023-09-09 20:11:43 +0100

    testing env variables for vercel deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ interface Blog {
 }
 
 export default async function Home() {
-  const res = await fetch("http://localhost:3000/api/blogs", {
+  const res = await fetch(process.env.URL + "/api/blogs", {
     cache: "no-store",
   });
   let data = await res.json();
