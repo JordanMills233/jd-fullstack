1862405653a4c5e2713ba00ec3a8d090d76f26a9
JordanMills233 <jordanmills233@gmail.com>
2023-09-25 20:24:42 +0100

    Updated E2E test to cover entire flow

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,5 @@
-describe("template spec", () => {
-    it("Create Blog Post Flow", () => {
+describe("Full website flow", () => {
+    it("Full Blog Post Flow", () => {
         cy.viewport(1920, 1080);
         cy.visit("https://jd-fullstack.vercel.app/");
         cy.contains("WELCOME TO MY BLOG");
@@ -10,10 +10,12 @@ describe("template spec", () => {
         cy.get("input").click().type("E2E Test Title");
         cy.get("textarea").click().type("E2E Test Description");
         cy.contains("Submit").click();
-        cy.contains("E2E Test Title").click();
-        cy.visit("https://jd-fullstack.vercel.app/");
-        cy.get("#E2ETestTitle").click();
         cy.wait(1000);
+        cy.contains("E2E Test Title").click();
+        cy.wait(5000);
+        cy.visit("https://jd-fullstack.vercel.app/");
+        cy.get("#E2ETestTitleedit").click();
+        cy.wait(3000);
         cy.get("div.cm-content")
             .click()
             .type("{enter}")
@@ -22,7 +24,10 @@ describe("template spec", () => {
         cy.get("input").click().type("Updated E2E Test Title");
         cy.get("textarea").click().type("Updated E2E Test Description");
         cy.contains("Submit").click();
+        cy.wait(1000);
         cy.contains("Updated E2E Test Title").click();
+        cy.wait(5000);
         cy.visit("https://jd-fullstack.vercel.app/");
+        cy.get("#UpdatedE2ETestTitledel").click();
     });
 });
